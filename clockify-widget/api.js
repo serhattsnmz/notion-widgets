a9e0a32b0a998319d01a4110e81ab3ce47d9b659
Serhat Sönmez <ssonmez@live.com>
2021-02-24 21:30:14 +0300

    add new widget

---
+++ clockify-widget/api.js
@@ -0,0 +1,124 @@
+class HelperFunctions {
+
+    static JsonToString(jsonData) {
+        return JSON.stringify(jsonData, null, 2)
+    }
+
+    static GetFirstDateOfCurrentWeek() {
+        var currentDay = new Date;
+        var firstDay = currentDay.getDate() - currentDay.getDay() + 1;
+        var weekFirstDay = new Date(currentDay.setDate(firstDay));
+        weekFirstDay = new Date(weekFirstDay.setHours(0, 0, 0, 0));
+
+        return weekFirstDay;
+    }
+
+    static GetLastDateOfCurrentWeek() {
+        var currentDay = new Date;
+        var lastDay = currentDay.getDate() - currentDay.getDay() + 7;
+        var weekLastDay = new Date(currentDay.setDate(lastDay));
+        weekLastDay = new Date(weekLastDay.setHours(23, 59, 59, 999));
+
+        return weekLastDay;
+    }
+
+    static ConvertSecondsToTime(totalSeconds) {
+        var hour = Math.trunc(totalSeconds / 3600);
+        var minute = Math.trunc((totalSeconds % 3600) / 60);
+        var seconds = totalSeconds % 60;
+
+        return `${hour.toString().padStart(2, "0")}:${minute.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+    }
+
+    static AddDays(date, days) {
+        var result = new Date(date);
+        result.setDate(result.getDate() + days);
+        return result;
+    }
+
+    static FormatDateForPrint(date) {
+        return `${date.getDate().toString().padStart(2, "0")}.${(date.getMonth() + 1).toString().padStart(2, "0")}`;
+    }
+}
+
+class ClockifyApi {
+
+    constructor(apiKey, workspaceId) {
+        this.apiKey = apiKey;
+        this.workspaceId = workspaceId;
+    }
+
+    getUserProjects() {
+        var ajaxResponse = null;
+        $.ajax({
+            async: false,
+            url: "https://api.clockify.me/api/v1/workspaces/" + this.workspaceId + "/projects",
+            type: "GET",
+            headers: {
+                "X-Api-Key": this.apiKey
+            },
+            success: function(response) {
+                ajaxResponse = response;
+            },
+            error: function(XMLHttpRequest, textStatus, errorThrown) {
+                console.log("Ajax error! <br />" + textStatus + "<br />" + jsonToString(errorThrown));
+            }
+        });
+
+        var _response = [];
+
+        ajaxResponse.forEach(element => {
+            _response.push({
+                "id": element["id"],
+                "name": element["name"]
+            })
+        });
+
+        return _response;
+    }
+
+    getWeeklyReport(startDate, endDate) {
+        var ajaxResponse = null;
+        $.ajax({
+            async: false,
+            url: "https://reports.api.clockify.me/v1/workspaces/" + this.workspaceId + "/reports/summary",
+            type: "POST",
+            headers: {
+                "X-Api-Key": this.apiKey,
+                "Content-Type": "application/json"
+            },
+            data: JSON.stringify({
+                "dateRangeStart": startDate,
+                "dateRangeEnd": endDate,
+                "summaryFilter": {
+                    "groups": [
+                        "PROJECT",
+                        "TIMEENTRY"
+                    ]
+                }
+            }),
+            success: function(response) {
+                ajaxResponse = response;
+            },
+            error: function(XMLHttpRequest, textStatus, errorThrown) {
+                console.log("Ajax error! <br />" + textStatus + "<br />" + jsonToString(errorThrown));
+            }
+        });
+
+        var _response = {
+            "totalTime": ajaxResponse["totals"][0] != null ?
+                HelperFunctions.ConvertSecondsToTime(ajaxResponse["totals"][0]["totalTime"]) : "-",
+            "projects": []
+        };
+
+        ajaxResponse["groupOne"].forEach(element => {
+            _response["projects"].push({
+                "id": element["_id"],
+                "totalTime": HelperFunctions.ConvertSecondsToTime(element["duration"]),
+                "name": element["name"]
+            })
+        });
+
+        return _response;
+    }
+}
---
+++ clockify-widget/readme.md
@@ -0,0 +1,33 @@
+# Clockify Widget
+
+## Query String Parameters:
+
+| Parameter     | Type      | Required  | Default   | Explanation   | Example           |
+| ---           | ----      | --------- | --------- | ------------- | -------           |
+| api-key       | string    | required  |           | Api key of account    | api-key=xxx       |
+| workspace-id  | string    | required  |           | Workspace ID          | workspcace-id=xxx |
+| week-count    | int       | optional  | 5         | How many week to show | week-count=5      |
+| work-limit    | string    | optional  |           | Set min work limit    | work-limit=20:00:00 |
+| font-size     | string    | optional  | 16px      | Table font size       | font-size=20px    |
+| dark-mode     | bool      | optional  | false     | Change dark mode      | dark-mode=true    |
+
+## Api Key
+
+Clockify API Key can be found on "Profile Settings > API > Api Key" section.
+
+## Workspace ID
+
+To get workspace id, go to `SETTINGS` page of workspace and workspace id will be shown on URL section of your browser. Example :
+
+```
+https://clockify.me/workspaces/<workspace-id>/settings#settings
+```
+
+## Work Limit:
+
+If you set work limit, color of week total time will change;
+
+- Red, if week total time is less than work limit;
+- Green, if week total time is greater than work limit
+
+Work limit format must be like "HH:mm:ss", for example "20:00:00" is 20hrs.
